9d7b87a39ec9b028c1d5c3cf2d4fc1494c5a1877
bigmooon <bigmooon1215@gmail.com>
2025-11-04 13:04:41 +0900

    feat(products): configure product feature module public API

---
+++ src/features/products/index.ts
@@ -0,0 +1,31 @@
+// Types
+export * from './types/product.type';
+export * from './types/review.type';
+
+// API
+export * from './api/products.api';
+export * from './api/reviews.api';
+
+// Hooks
+export * from './hooks/use-products';
+export * from './hooks/use-reviews';
+export * from './hooks/use-image-error';
+export * from './hooks/use-full-product-data';
+
+// Stores
+export * from './stores/use-product-store';
+
+// Utils
+export * from './utils';
+
+// Constants
+export * from './constants/image.constants';
+
+// Components
+export * from './components/product-image';
+export * from './components/product-card';
+export * from './components/mall-card';
+export * from './components/badge-list';
+export * from './components/price-section';
+export * from './components/review-section';
+export * from './components/skeletons';
