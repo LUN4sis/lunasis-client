871bc1c826ec107c04dbf80370d2ff65851a8b3e
bigmooon <bigmooon1215@gmail.com>
2025-11-28 03:47:26 +0900

    chore(web): add light font weight

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -8,7 +8,7 @@ import './globals.scss';
 const dmSans = DM_Sans({
   variable: '--font-dm-sans',
   subsets: ['latin'],
-  weight: ['400', '500', '600', '700'],
+  weight: ['300', '400', '500', '600', '700'],
 });
 
 export const metadata: Metadata = {
