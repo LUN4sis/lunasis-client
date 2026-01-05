090c5ce63046d912c869813210ec0706e645c5b2
jpaper <bigmooon1215@gmail.com>
2026-01-06 08:21:09 +0900

    fix: add missing type product

---
+++ packages/shared/src/features/products/index.ts
@@ -0,0 +1,19 @@
+export type {
+  Product,
+  ProductPrice,
+  ProductDetail,
+  Mall,
+  GetProductsParams,
+  GetProductDetailParams,
+  GetProductBundleParams,
+  GetProductsResponse,
+  GetProductDetailResponse,
+  GetProductBundleResponse,
+  ProductFilters,
+  ProductStore,
+} from './types';
+
+export { ProductCategory } from './types';
+
+// Stores
+export { useProductStore } from './stores';
