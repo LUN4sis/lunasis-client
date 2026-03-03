4fec769516c5e78e86acca2df71d67f9e4fdf2cc
Junghee Im(Jion Im) <imjion.dev@gmail.com>
2026-03-03 14:18:16 +0900

    refactor(pwa): remove unused PWA components and utils

--- apps/web/src/features/pwa/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as InstallPrompt } from './components/install-prompt';
-export { registerServiceWorker } from './services/register-service-worker';
-export * from './utils/pwa.utils';
